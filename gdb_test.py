8174bea3f27f9379aa6077cea92c18b5597aa3f0
Fasten90 <fasten90@gmail.com>
2022-02-19 22:04:00 +0100

    CI: systemtest - fix 10 - Revert removing argument

--- gdb_test.py
+++ gdb_test.py
@@ -142,9 +142,9 @@ def check_and_prepare(test_elf_path, qemu_path):
 
     try:
         # Test: qemu-system-gnuarmeclipse.exe --version
-        #qemu_test_args = '--version'
-        print('Test: {}'.format(qemu_path))
-        proc_qemu_test = subprocess.run([qemu_path],
+        qemu_test_args = '--version'
+        print('Test: {} {}'.format(qemu_path, qemu_test_args))
+        proc_qemu_test = subprocess.run([qemu_path, qemu_test_args],
                                           shell=True,
                                           capture_output=True)
     except Exception as ex:
