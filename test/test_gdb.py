9a7e36d43c6372f32e7d19a3571e4176ef911004
Fasten90 <fasten90@gmail.com>
2022-02-22 00:10:41 +0100

    Try to fix system test - 5. - syntax fir for list-str

--- test/test_gdb.py
+++ test/test_gdb.py
@@ -9,37 +9,21 @@ from sys import platform
 import gdb_test
 
 
-def get_python_by_platform():
-    python_bin = 'python'
-    if platform == 'linux' or platform == 'linux2':
-        # linux
-        python_bin = 'python3'
-    elif platform == 'darwin':
-        # OS X
-        python_bin = 'python3'
-    elif platform == 'win32':
-        # Do nothing with the command
-        python_bin = 'python'
-    return python_bin
-
-
 class TestReference(unittest.TestCase):
     def test_reference(self):
         import subprocess
 
         python_file = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'gdb_test.py'))
         test_file_path = os.path.dirname(__file__) + '/' + 'reference' + '/' + 'FastenHomeAut'
-        args = ['--test_file_path', '{}'.format(test_file_path)]
+        arg_list = ['--test_file_path', '{}'.format(test_file_path)]
 
         # If pipeline
         is_pipeline = os.getenv('PIPELINE_WORKSPACE')
         if is_pipeline:
             QEMU_BIN_PATH = os.getenv('QEMU_BIN_PATH')
-            args += ['--qemu_bin_path', './{qemu_bin_path}'.format(qemu_bin_path=QEMU_BIN_PATH)]
+            arg_list += ['--qemu_bin_path', './{qemu_bin_path}'.format(qemu_bin_path=QEMU_BIN_PATH)]
 
-        python = get_python_by_platform()
-        #subprocess.run(['python', python_file + ' ' + args])
-        exec_command = [sys.executable, python_file] + [args]
+        exec_command = [sys.executable, python_file] + arg_list
         #p = subprocess.Popen(exec_command, shell=True, stdout=subprocess.PIPE)
         #out, err = p.communicate()
 
