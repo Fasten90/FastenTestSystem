15a5dfe4afc7250b2acac711f591bf961a8e74cd
Fasten90 <fasten90@gmail.com>
2022-02-21 23:59:04 +0100

    Try to fix system test - 4. - syntax fix

--- test/test_gdb.py
+++ test/test_gdb.py
@@ -49,7 +49,7 @@ class TestReference(unittest.TestCase):
         #        print(line, end='')  # process line here
         print('In the ideal world we execute this: {}'.format(exec_command))
         
-        exec_command_str = ''.join([' ' + item for item in exec_command]
+        exec_command_str = ''.join([' ' + item for item in exec_command])
         test_execution = subprocess.run(exec_command_str, capture_output=True) # 'shell=True' maybe not necessary
         # Removed 'stdout=True, ' due 'ValueError: stdout and stderr arguments may not be used with capture_output.' error
         
