f45ef6e94fa1cdd098e229503202281928c54ced
Fasten90 <fasten90@gmail.com>
2022-02-21 23:51:11 +0100

    Try to fix system test - 2.

--- test/test_gdb.py
+++ test/test_gdb.py
@@ -29,17 +29,17 @@ class TestReference(unittest.TestCase):
 
         python_file = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'gdb_test.py'))
         test_file_path = os.path.dirname(__file__) + '/' + 'reference' + '/' + 'FastenHomeAut'
-        args = '--test_file_path {}'.format(test_file_path)
+        args = ['--test_file_path', '{}'.format(test_file_path)]
 
         # If pipeline
         is_pipeline = os.getenv('PIPELINE_WORKSPACE')
         if is_pipeline:
             QEMU_BIN_PATH = os.getenv('QEMU_BIN_PATH')
-            args += ' ' + '--qemu_bin_path=./{qemu_bin_path}'.format(qemu_bin_path=QEMU_BIN_PATH)
+            args += ['--qemu_bin_path', './{qemu_bin_path}'.format(qemu_bin_path=QEMU_BIN_PATH)]
 
         python = get_python_by_platform()
         #subprocess.run(['python', python_file + ' ' + args])
-        exec_command = [sys.executable, python_file, args]
+        exec_command = [sys.executable, python_file] + [args]
         #p = subprocess.Popen(exec_command, shell=True, stdout=subprocess.PIPE)
         #out, err = p.communicate()
 
